fc1d21df1101a4f74f134399c20a48eb02b1a87e
carmen0208 <13770576@qq.com>
2016-06-20 18:11:52 +0800

    04Template with hacked way instead of Template

--- Template/report_demo.rb
+++ Template/report_demo.rb
@@ -1,13 +1,38 @@
 #!/usr/bin/env ruby
 
-require_relative '../example'
-
+##remeber to install minitest using gem install minitest
+require 'minitest/autorun'
+require 'pp'
+require 'stringio'
 
 require_relative 'report'
 
-example %q{
 
-report = Report.new
-report.output_report
+class HackedTest < MiniTest::Unit::TestCase
 
-}
+  def test_hacked_report
+    r = Report.new
+    output = capture_output { r.output_report(:plain) }
+puts output
+    assert /Things/ =~ output
+
+    output = capture_output { r.output_report(:html) }
+puts output
+    assert /<html>/ =~ output
+    assert /<title>Monthly/ =~ output
+    assert_raise(RuntimeError){ r.output_report(:pdf) }
+
+  end
+
+  def capture_output(&block)
+    output = StringIO.new
+    begin
+       $stdout = output
+       block.call
+    ensure
+       $stdout = STDOUT
+    end
+    output.string
+  end
+
+end
