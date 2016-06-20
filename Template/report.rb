6ea949beef098b47881a61c0605cdcd27ad0d3bd
carmen0208 <13770576@qq.com>
2016-06-20 18:24:06 +0800

    5th Template Refactor, hooking method

--- Template/report.rb
+++ Template/report.rb
@@ -1,4 +1,3 @@
-
 class Report
   def initialize
     @title = 'Monthly Report'
@@ -20,7 +19,7 @@ class Report
   end
 
   def output_head
-    raise 'Called abstract method: output_head'
+    output_line(@title)
   end
 
   def output_body_start
@@ -39,47 +38,8 @@ end
 
 
 
-class HTMLReport < Report
-  def output_start
-    puts('<html>')
-  end
-
-  def output_head
-    puts('  <head>')
-    puts("    <title>#{@title}</title>")
-    puts('  </head>')
-  end
-
-  def output_body_start
-    puts('<body>')
-  end
-
-  def output_line(line)
-    puts("  <p>#{line}</p>")
-  end
-
-  def output_body_end
-    puts('</body>')
-  end
-
-  def output_end
-    puts('</html>')
-  end
-end
-
 class PlainTextReport < Report
-  def output_start
-  end
-
-  def output_head
-    puts("**** #{@title} ****")
-    puts
-  end
-
   def output_line(line)
     puts(line)
-  end
-
-  def output_end
   end
 end
--- Template/report_demo.rb
+++ Template/report_demo.rb
@@ -8,28 +8,23 @@ require 'stringio'
 require_relative 'report'
 
 
-class HookReportTest <  Minitest::Test
+class DosomethingHookReportTest <  Minitest::Test
 
-  def test_hacked_report
-    r = PlainTextReport.new
-    output = capture_output { r.output_report }
-    assert /Things/ =~ output
-
-    r = HTMLReport.new
-    output = capture_output { r.output_report }
-    assert /<html>/ =~ output
-    assert /<title>Monthly/ =~ output
-  end
-
-  def capture_output(&block)
-    output = StringIO.new
-    begin
-       $stdout = output
-       block.call
-    ensure
-       $stdout = STDOUT
+    def test_hacked_report
+      r = PlainTextReport.new
+      output = capture_output { r.output_report }
+      assert /Things/ =~ output
     end
-    output.string
-  end
 
-end
+    def capture_output(&block)
+      output = StringIO.new
+      begin
+         $stdout = output
+         block.call
+      ensure
+         $stdout = STDOUT
+      end
+      output.string
+    end
+
+  end
