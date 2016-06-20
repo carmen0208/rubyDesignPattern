5eebeb5ea8cd7f5e1391705610e2072d286b98b4
carmen0208 <13770576@qq.com>
2016-06-20 18:20:49 +0800

    3rd Template Refactor, with Template

--- Template/report_demo.rb
+++ Template/report_demo.rb
@@ -1,38 +1,13 @@
 #!/usr/bin/env ruby
 
-##remeber to install minitest using gem install minitest
-require 'minitest/autorun'
-require 'pp'
-require 'stringio'
-
+require_relative '../example'
 require_relative 'report'
 
+example %q{
+report = HTMLReport.new
+report.output_report
 
-class HackedTest < MiniTest::Unit::TestCase
+report = PlainTextReport.new
+report.output_report
 
-  def test_hacked_report
-    r = Report.new
-    output = capture_output { r.output_report(:plain) }
-puts output
-    assert /Things/ =~ output
-
-    output = capture_output { r.output_report(:html) }
-puts output
-    assert /<html>/ =~ output
-    assert /<title>Monthly/ =~ output
-    assert_raise(RuntimeError){ r.output_report(:pdf) }
-
-  end
-
-  def capture_output(&block)
-    output = StringIO.new
-    begin
-       $stdout = output
-       block.call
-    ensure
-       $stdout = STDOUT
-    end
-    output.string
-  end
-
-end
+}
