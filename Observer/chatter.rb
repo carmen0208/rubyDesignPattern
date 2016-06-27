dfa72f10a518a700055ec02a3c7eb017fbccc424
carmen0208 <13770576@qq.com>
2016-06-27 18:26:42 +0800

    Observer:consistent_demo

--- Observer/chatter.rb
+++ Observer/chatter.rb
@@ -14,21 +14,19 @@ class Employee
   end
 
   def salary=(new_salary)
-    old_salary = @salary
-    @salary = new_salary
-    if old_salary != new_salary
-      changed
-      notify_observers(self)
-    end
-  end
-  
-  def title=(new_title)
-    old_title = @title
-    @title = new_title
-    if old_title != new_title
-      changed = true
-      notify_observers(self)
-    end
-  end
+     old_salary = @salary
+     @salary = new_salary
+     changed if old_salary != new_salary
+   end
+
+   def title=(new_title)
+     old_title = @title
+     @title = new_title
+     changed if old_title != new_title
+   end
+
+   def changes_complete
+     notify_observers(self)
+   end
 
 end
--- Observer/chatter_test.rb
+++ Observer/chatter_test.rb
@@ -21,27 +21,18 @@ end
 
 class LessChattyObserverTest < Minitest::Test
 
-  def test_salary_chattyness
+  def test_consistent_changes
     fred = Employee.new("fred", "title", 30000)
     observer = Observer.new
     fred.add_observer(observer)
 
-    fred.salary = 30000
-    assert_equal 0, observer.subjects.size
-
-    fred.salary = 30001
-    assert_equal fred, observer.subjects[0]
-  end
-
-  def test_title_chattyness
-    fred = Employee.new("fred", "title", 30000)
-    observer = Observer.new
-    fred.add_observer(observer)
-
-    fred.title = 'title'
+    fred.salary = 60000
     assert_equal 0, observer.subjects.size
 
     fred.salary = 'president'
+    assert_equal 0, observer.subjects.size
+
+    fred.changes_complete
     assert_equal fred, observer.subjects[0]
   end
 
