232cd7075076ae1bf8967b4c2a315c4200987354
carmen0208 <13770576@qq.com>
2016-06-27 18:15:03 +0800

    Step 7:forget about employee_demo.rb, added it

--- Observer/employee_demo.rb
+++ Observer/employee_demo.rb
@@ -4,24 +4,16 @@ require_relative "../example"
 require_relative "employee"
 
 example %q{
-payroll = Payroll.new
-carmen = Employee.new("Carmen Ninja Liu", "Ninja of the World", 1000000.0, payroll)
 
-carmen.add_observer(payroll)
+carmen = Employee.new("Carmen Ninja Liu", "Ninja of the World", 1000000.0)
+
+
+carmen.add_observer do |changed_employee|
+    puts "Cut a new check for #{changed_employee.name}!"
+    puts "His salary is now #{changed_employee.salary}!"
+end
 
 carmen.salary = 7000000.0
 
-
-class TaxMan
-  def update(changed_employee)
-    puts "Send #{changed_employee.name} a new tax bill!"
-  end
-end
-
-tax_man = TaxMan.new
-
-carmen.add_observer(tax_man)
-
-carmen.salary = 1000000.0
 
 }
