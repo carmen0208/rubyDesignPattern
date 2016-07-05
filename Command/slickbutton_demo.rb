642657eb0221453ee172e0a229314d56aa2346b1
carmen0208 <13770576@qq.com>
2016-07-05 09:23:16 +0800

    Command::Step1: An Explosion of Subclasses, bad practice

---
+++ Command/slickbutton_demo.rb
@@ -0,0 +1,51 @@
+#!/usr/bin/env ruby
+require_relative '../example'
+
+example %q{
+
+class SlickButton
+  #
+  # Lots of button drawing and management
+  # code omitted...
+  #
+
+  def on_button_push
+    #
+    # Do something when the button is pushed.
+    #
+  end
+end
+
+}
+
+example %q{
+class SaveButton < SlickButton
+  def on_button_push
+    #
+    # Save the current document...
+    #
+  end
+end
+
+class NewDocumentButton < SlickButton
+  def on_button_push
+    #
+    # Create a new document...
+    #
+  end
+end
+}
+
+example %q{
+sb = SlickButton.new
+sb.on_button_push
+pp sb
+
+sb = SaveButton.new
+sb.on_button_push
+pp sb
+
+ndb = NewDocumentButton.new
+ndb.on_button_push
+pp ndb
+}
