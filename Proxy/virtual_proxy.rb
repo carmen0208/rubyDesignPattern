d127ddcf5e1073f82092d0525b80c977364f277a
carmen0208 <13770576@qq.com>
2016-07-08 09:03:29 +0800

    Proxy::Step 8: Virtual proxy with method missing theory

--- Proxy/virtual_proxy.rb
+++ Proxy/virtual_proxy.rb
@@ -6,22 +6,29 @@ class VirtualAccountProxy
     @creation_block= creation_block
   end
 
-  def deposit(amount)
+  # def deposit(amount)
+  #   s = subject
+  #   s.deposit(amount)
+  # end
+  #
+  # def withdraw(amount)
+  #   s = subject
+  #   s.withdraw(amount)
+  # end
+  #
+  # def balance
+  #   s = subject
+  #    s.balance
+  # end
+
+  def method_missing(name, *args)
     s = subject
-    s.deposit(amount)
+    s.send( name, *args)
   end
 
-  def withdraw(amount)
-    s = subject
-    s.withdraw(amount)
-  end
-
-  def balance
-    s = subject
-     s.balance
-  end
   def subject
     # puts @start_balance
-    @subject || (@subject = @creation_block.call)
+    @subject || (@subject = @creation_block.call) unless @subject
+    @subject
   end
 end
--- Proxy/virtual_proxy_demo.rb
+++ Proxy/virtual_proxy_demo.rb
@@ -4,11 +4,19 @@ require_relative '../example'
 require_relative 'virtual_proxy'
 require_relative 'account'
 
+# example %q{
+#
+# account = VirtualAccountProxy.new { BankAccount.new(10) }
+#
+# pp account
+# account.deposit(1)
+# pp account
+# }
 example %q{
 
-account = VirtualAccountProxy.new { BankAccount.new(10) }
+array = VirtualAccountProxy.new { Array.new}
 
-pp account
-account.deposit(1)
-pp account
+array << 'hello'
+array << 'out'
+array << 'there'
 }
